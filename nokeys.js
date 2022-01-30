fa60510c48176fd17f2fe0bae13dd6003868f94e
Yuval Ben-Hayun <ybenhayun@gmail.com>
2022-01-30 14:39:42 -0500

    button presses

--- nokeys.js
+++ nokeys.js
@@ -10,7 +10,6 @@ $(document).ready(function(){
             var letter = String.fromCharCode(e.which).toUpperCase();
             $("#" + letter).removeClass('backspace');
             $("#" + letter).addClass('keypress');
-            // changeCSS("#" + letter)
 
             if (!used.includes(letter)) $("#word").append(letter);
         } else if (e.keyCode == 32 || e.keyCode == 13) {
@@ -27,16 +26,59 @@ $(document).ready(function(){
                         if (!used.includes(letter)) used.push(letter);
                         $("#" + letter).remove();
                     } else {
-                        // resetCSS("#" + letter);
                         $("#" + letter).removeClass('keypress');
                     }
                 }
             }
 
             score = 26 - used.length;
-            $(".points").html(score + " letters")
+            $(".points").html(score + " letters");
         }
     });
+    
+    $(".key").click(function() {
+        var key = $(this).children().first().attr('id');
+        console.log(key);
+
+        if (key == 'clear') {
+            var word = $("#word").html();
+            var letter = word.at(-1);
+            $("#word").html(word.slice(0, -1));
+
+            if (!$("#word").html().includes(letter)) {
+                $("#" + letter).removeClass('keypress');
+                $("#" + letter).addClass('backspace');
+            }
+        } else if (key == 'enter') {
+            if (!word_list.includes($("#word").html()) && !word_list.includes($("#word").html().toLowerCase())) {
+                var is_word = false;
+            } else is_word = true;
+            $("#word").empty();
+
+            for (var i = 65; i <= 90; i++) {
+                var letter = String.fromCharCode(i).toUpperCase();
+
+                if ($("#" + letter).css("background-color") != "rgb(43, 43, 43)") {
+                    if (is_word) {
+                        if (!used.includes(letter)) used.push(letter);
+                        $("#" + letter).remove();
+                    } else {
+                        $("#" + letter).removeClass('keypress');
+                    }
+                }
+            }
+
+            score = 26 - used.length;
+            $(".points").html(score + " letters");
+        } else {
+            $("#" + key).removeClass('backspace');
+            $("#" + key).addClass('keypress');
+            
+            if (!used.includes(key)) $("#word").append(key);
+        }
+    });
+
+
 
     $(document).keyup(function(e) {
         if (e.keyCode == 8) {
@@ -45,9 +87,6 @@ $(document).ready(function(){
             $("#word").html(word.slice(0, -1));
 
             if (!$("#word").html().includes(letter)) {
-                // $("." + letter).css({height: "100%" });
-                // $("." + letter).css({background: "#2b2b2b"});
-                // resetCSS("#" + letter);
                 $("#" + letter).removeClass('keypress');
                 $("#" + letter).addClass('backspace');
             }
@@ -58,18 +97,6 @@ $(document).ready(function(){
         if (e.keyCode != 13 || e.keyCode != 32) restart();
     });
 });
-
-function changeCSS(id) {
-    $(id).css({ background: "blue" });
-    $(id).css({ height: "80%" });
-    $(id).css({ fontSize: ".8rem" });
-}
-
-function resetCSS(id) {
-    $(id).css({ background: "#2b2b2b"});
-    $(id).css({ height: "100%" });
-    $(id).css({ fontSize: "1rem" });
-}
 
 function restart() {
 
@@ -85,6 +112,9 @@ function restart() {
         }
         $(row).append("<div class = 'key'><div id = " + letters[i] + " class = 'letter'> " + letters[i] + " </div></div>");
     }
+
+    $('.bottom').append("<div class = 'key'><div id = 'clear'><i class='material-icons' style='font-size:1rem'>backspace</i></div></div>");
+    $('.bottom').prepend("<div class = 'key'><div id = 'enter'> ENTER</div></div>")
 
     score = 26;
     used = [];
