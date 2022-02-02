97471dcb4e831b1b9f0d3c9f0d5cded79e203b62
Yuval Ben-Hayun <ybenhayun@gmail.com>
2022-02-01 22:52:30 -0500

    phone edition

--- nokeys.js
+++ nokeys.js
@@ -8,98 +8,88 @@ $(document).ready(function(){
     $(document).keypress(function(e) {
         if ((e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 97 && e.keyCode <= 122)) {
             var letter = String.fromCharCode(e.which).toUpperCase();
-            $("#" + letter).removeClass('backspace');
-            $("#" + letter).addClass('keypress');
-
-            if (!used.includes(letter)) $("#word").append(letter);
-        } else if (e.keyCode == 32 || e.keyCode == 13) {
-            if (!word_list.includes($("#word").html()) && !word_list.includes($("#word").html().toLowerCase())) {
-                var is_word = false;
-            } else is_word = true;
-            $("#word").empty();
-
-            for (var i = 65; i <= 90; i++) {
-                var letter = String.fromCharCode(i).toUpperCase();
-
-                if ($("#" + letter).css("background-color") != "rgb(43, 43, 43)") {
-                    if (is_word) {
-                        if (!used.includes(letter)) used.push(letter);
-                        $("#" + letter).remove();
-                    } else {
-                        $("#" + letter).removeClass('keypress');
-                    }
-                }
-            }
-
-            score = 26 - used.length;
-            $(".points").html(score + " letters");
+            pressedLetter(letter);
+        } else if (e.keyCode == 13) {
+            hitEnter($("#word").html());
+        } else if (e.keyCode == 32) {
+            restart();
         }
     });
     
-    $(".key").click(function() {
+    $(document).on('click', '.key', function() {
         var key = $(this).children().first().attr('id');
-        console.log(key);
 
         if (key == 'clear') {
-            var word = $("#word").html();
-            var letter = word.at(-1);
-            $("#word").html(word.slice(0, -1));
-
-            if (!$("#word").html().includes(letter)) {
-                $("#" + letter).removeClass('keypress');
-                $("#" + letter).addClass('backspace');
-            }
+            hitDelete($("#word").html());
         } else if (key == 'enter') {
-            if (!word_list.includes($("#word").html()) && !word_list.includes($("#word").html().toLowerCase())) {
-                var is_word = false;
-            } else is_word = true;
-            $("#word").empty();
-
-            for (var i = 65; i <= 90; i++) {
-                var letter = String.fromCharCode(i).toUpperCase();
-
-                if ($("#" + letter).css("background-color") != "rgb(43, 43, 43)") {
-                    if (is_word) {
-                        if (!used.includes(letter)) used.push(letter);
-                        $("#" + letter).remove();
-                    } else {
-                        $("#" + letter).removeClass('keypress');
-                    }
-                }
-            }
-
-            score = 26 - used.length;
-            $(".points").html(score + " letters");
+            hitEnter($("#word").html());
+        } else if (key == 'spacebar') {
+            restart();  
         } else {
-            $("#" + key).removeClass('backspace');
-            $("#" + key).addClass('keypress');
-            
-            if (!used.includes(key)) $("#word").append(key);
+            pressedLetter(key);
         }
     });
-
-
 
     $(document).keyup(function(e) {
         if (e.keyCode == 8) {
-            var word = $("#word").html();
-            var letter = word.at(-1);
-            $("#word").html(word.slice(0, -1));
-
-            if (!$("#word").html().includes(letter)) {
-                $("#" + letter).removeClass('keypress');
-                $("#" + letter).addClass('backspace');
-            }
+            hitDelete($("#word").html());
         }
     });
 
-    $("button").click(function(e) {
-        if (e.keyCode != 13 || e.keyCode != 32) restart();
-    });
+
+    $(window).resize(function() {
+        setFontSize();
+    })
 });
 
-function restart() {
+function setFontSize() {
+    console.log($("#keyboard").height());
+}
 
+function pressedLetter(letter) {
+    $("#" + letter).removeClass('backspace');
+    $("#" + letter).addClass('keypress');
+            
+    if (!used.includes(letter)) $("#word").append(letter);
+}
+
+function hitEnter(word) {
+    if (!word_list.includes(word) && !word_list.includes(word.toLowerCase())) {
+        var is_word = false;
+    } else is_word = true;
+
+    $("#word").empty();
+    
+    for (var i = 0; i <= word.length; i++) {
+        var letter = word.charAt(i);
+        if (is_word) {
+            if (!used.includes(word.charAt(i)) && word.charAt(i) != '') { 
+                used.push(word.charAt(i));
+            }
+
+            $("#" + word.charAt(i)).removeClass('keypress');
+            $("#" + word.charAt(i)).addClass('disappear');
+        } else {
+            $("#" + word.charAt(i)).removeClass('keypress');
+            $("#" + word.charAt(i)).addClass('backspace');
+        }
+    }
+    
+    score = 26 - used.length;
+    $(".points").html(score + " letters");
+}
+
+function hitDelete(word) {
+    var letter = word.at(-1);
+    $("#word").html(word.slice(0, -1));
+
+    if (!$("#word").html().includes(letter)) {
+        $("#" + letter).removeClass('keypress');
+        $("#" + letter).addClass('backspace');
+    }
+}
+
+function restart() {
     var row = ".top";
     $(row).empty();
 
@@ -112,12 +102,14 @@ function restart() {
         }
         $(row).append("<div class = 'key'><div id = " + letters[i] + " class = 'letter'> " + letters[i] + " </div></div>");
     }
-
-    $('.bottom').append("<div class = 'key'><div id = 'clear'><i class='material-icons' style='font-size:1rem'>backspace</i></div></div>");
-    $('.bottom').prepend("<div class = 'key'><div id = 'enter'> ENTER</div></div>")
+    $('.funcs').empty();
+    $('.funcs').append("<div class = 'key'><div id = 'enter'>ENTER</div></div>");
+    $('.funcs').append("<div class = 'key'><div id = 'spacebar'>RESET</div></div>")
+    $('.funcs').append("<div class = 'key'><div id = 'clear'><i class='material-icons' style='font-size:1rem'>backspace</i></div></div>");
 
     score = 26;
     used = [];
     $(".points").remove();
     $("#score").append("<div class = 'points'>" + score + " letters</div>")
+    $("#word").empty();
 }
